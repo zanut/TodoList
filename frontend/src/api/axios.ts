09962d9654ff2d2612a7749d8c6a722096da4ec6
Chanun Suntrapiwat <chanun.s@ku.th>
2025-04-21 02:12:17 +0700

    add env example file

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:8080",
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 API.interceptors.request.use((config) => {
